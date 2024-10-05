cf60422457e82ce14d27526ec5402364838fb875
Sujan Baskota <sujan.baskota77@gmail.com>
2024-10-05 13:19:03 +0545

    fixed exoplanet view

---
+++ src/components/Exoplanet/FloatingSpheres.jsx
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from "react";
+import { Canvas, useThree, extend, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { TextureLoader } from "three";
+import * as THREE from "three";
+
+const FloatingSpheres = () => {
+  const spheres = [];
+
+  for (let i = 0; i < 10; i++) {
+    const position = [
+      (Math.random() - 0.5) * 50,
+      (Math.random() - 0.5) * 50 + 1, // Adjusted to ensure spheres are above the plane
+      (Math.random() - 0.5) * 50,
+    ];
+    spheres.push(
+      <mesh position={position} key={i}>
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    );
+  }
+
+  return <>{spheres}</>;
+};
+
+
+export default FloatingSpheres
